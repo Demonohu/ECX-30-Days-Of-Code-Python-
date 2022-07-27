201236514fd39970687d5ba5fad4ea9a31f16df5
Demonohu <ademonohu@gmail.com>
2022-07-28 00:33:16 +0100

    This is a guessing the game. It allows the user to guess a number in a maximum of 5 tries. If the guess is wrong, it returns an error message indicating whether the guess is greater or lesser than the hidden number.

---
+++ Day 14/guess_the_no.py
@@ -0,0 +1,61 @@
+#Guess the number
+#You ask a user to guess a number between 1 and 50.
+#The user has a maximum of 5 tries. If the user guesses
+#wrongly, provide an error message indicating whether 
+#their guess was above or below the actual number.
+#If the user guesses correctly, congratulate them 
+#and show the number of attempts they had. If the
+#user exhausts all their tries, and end the game.
+#e.g; 
+#> enter a number
+#user: 1
+#> Wrong. The answer is greater than 1
+#user: 25
+#> Wrong. The answer is less than 25.
+#user: 14
+#> Wrong.The aanswer is greater than 14.
+#user: 15
+#> Correct! You got the right answer in 3 tries.
+
+import random
+
+def guess():
+    while True:
+        try:
+            user_pick = int(input('Enter an integer between 0 and 100: '))
+            if user_pick < 0 or user_pick > 100:
+                print('The number must be between 0 and 100.\n')
+                continue
+        except ValueError:
+            print('Number must be in figures not letters.\n')
+            continue
+        break
+    return user_pick
+
+def main():
+    the_number = random.randint(0, 100)
+    user_pick = guess()
+    no_of_trials = 0
+    for tries in range(4):
+        if user_pick < the_number:
+            print('Wrong.The answer is greater than', user_pick)
+            print()
+            user_pick = guess()
+            print()
+            no_of_trials += 1
+        elif user_pick > the_number:
+            print('Wrong.The answer is less than', user_pick)
+            print()
+            user_pick = guess()
+            print()
+            no_of_trials += 1
+        else:
+            if no_of_trials > 1:
+                print('Correct! You got the number in', no_of_trials, 'tries.')
+            else:
+                print('Correct! You got the number in', no_of_trials, 'try.')
+    else:
+        print('Wrong. You have exhausted your tries.\nGame over!')
+        print('The number is', the_number)
+
+main()
